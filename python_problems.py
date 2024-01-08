407d7f5f43cec893ed3487b99577f3b932f6d8c9
rencdr <demirerencan@outlook.com>
2024-01-08 22:10:49 +0300

    problem 4 added

--- python_problems.py
+++ python_problems.py
@@ -29,3 +29,21 @@ def is_palindrome(x):
 
 
 """
+
+"""
+4.Roman to Integer- Convert a Roman numeral to an integer. Input is guaranteed to be within the range from 1 to 3999.
+
+def roman_to_int(s):
+    roman_dict = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+    result = 0
+    prev_value = 0
+    for char in s:
+        value = roman_dict[char]
+        result += value
+        if value > prev_value:
+            result -= 2 * prev_value
+        prev_value = value
+    return result
+
+
+"""
