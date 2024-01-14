e834b5b5d45f00a2ee2ce031b368780da5b10ae1
rencdr <demirerencan@outlook.com>
2024-01-14 23:52:46 +0300

    problem 8 added

--- python_problems.py
+++ python_problems.py
@@ -79,3 +79,24 @@ def abbrev_name(name):
     initials = words[0][0].upper() + '.' + words[1][0].upper()
     return initials
 """
+"""
+8.You have a long flowerbed in which some of the plots are planted, and some are not. However, flowers cannot be planted in adjacent plots.
+Given an integer array flowerbed containing 0's and 1's, where 0 means empty and 1 means not empty, and an integer n, return true if n new flowers can be planted in the flowerbed without violating the no-adjacent-flowers rule and false otherwise.
+
+ from typing import List
+
+class Solution:
+    def canPlaceFlowers(self, flowerbed: List[int], n: int) -> bool:
+        i = 0
+        while i < len(flowerbed):
+            if flowerbed[i] == 0:
+                if i == len(flowerbed) - 1 or flowerbed[i + 1] == 0:
+                    n -= 1  
+                    i += 2  
+                else:
+                    i += 1  
+            else:
+                i += 2
+        
+        return n <= 0
+"""
