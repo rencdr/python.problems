591d3e2a79afd1c4220e998ca609015768f8cb5d
rencdr <demirerencan@outlook.com>
2024-01-20 21:55:16 +0300

    problem 16 added

--- python_problems.py
+++ python_problems.py
@@ -214,3 +214,18 @@ def points(games):
     return total_points
 
 """
+"""
+16.An isogram is a word that has no repeating letters, consecutive or non-consecutive. Implement a function that determines whether a string that contains only letters is an isogram. Assume the empty string is an isogram. Ignore letter case.
+
+def is_isogram(string):
+    string = string.lower()
+    
+    seen = set()
+    
+    for char in string:
+        if char in seen:
+            return False
+        seen.add(char)
+    
+    return True
+"""
