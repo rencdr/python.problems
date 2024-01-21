402b4ba64f36be54f348c00ff64aec10daad0649
rencdr <demirerencan@outlook.com>
2024-01-21 11:51:19 +0300

    problem 20 added

--- python_problems.py
+++ python_problems.py
@@ -268,3 +268,18 @@ def double_integer(i):
     total = (i*2);
     return total;
 """
+"""
+20.Return the number (count) of vowels in the given string.
+We will consider a, e, i, o, u as vowels for this Kata (but not y).
+The input string will only consist of lower case letters and/or spaces.
+
+def get_count(sentence):
+    vowelsCount = 0
+    vowels = ['a', 'e', 'i', 'o', 'u']
+    
+    for char in sentence:
+        if char in vowels:
+            vowelsCount += 1
+    
+    return vowelsCount
+"""
